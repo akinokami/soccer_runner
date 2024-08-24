51c9327d2644df9b0e222e35e005c30c7f5e88b5
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-08-24 13:13:59 +0800

    a

---
+++ lib/screens/game_screen.dart
@@ -0,0 +1,167 @@
+import 'package:flame/camera.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:soccer_run/game/soccer_run.dart';
+import 'package:soccer_run/screens/widget/custom_game_button.dart';
+import 'package:soccer_run/screens/widget/custom_text.dart';
+import 'package:soccer_run/utils/app_theme.dart';
+import 'package:soccer_run/widgets/hud.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../services/local_storage.dart';
+import '../utils/constants.dart';
+import '../utils/global.dart';
+import '../widgets/game_over_menu.dart';
+import '../widgets/main_menu.dart';
+import '../widgets/pause_menu.dart';
+import '../widgets/settings_menu.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  bool isAccepted = false;
+  bool isChecked = false;
+  String f = '';
+  int selectedGame = 4;
+
+  @override
+  void initState() {
+    super.initState();
+
+    f = LocalStorage.instance.read(first) ?? '';
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      try {
+        if (f == '') {
+          if (context.mounted) {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (ctx) => Builder(builder: (context) {
+                return StatefulBuilder(
+                  builder: (context, StateSetter setState) {
+                    return AlertDialog(
+                      content: Column(
+                        children: [
+                          SingleChildScrollView(
+                            child: SizedBox(
+                                height: 1.sh * 0.55,
+                                width: double.infinity,
+                                child: WebViewWidget(
+                                    controller: WebViewController()
+                                      ..loadHtmlString(Global.language == "vi"
+                                          ? Global.policyHtmlVi
+                                          : Global.policyHtmlEn))),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Checkbox(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(6)),
+                                activeColor: Colors.orange,
+                                side: BorderSide(
+                                  width: 1.5,
+                                  color:
+                                      isChecked ? Colors.orange : Colors.black,
+                                ),
+                                value: isChecked,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    isChecked = value!;
+                                    if (isChecked) {
+                                      isAccepted = true;
+                                    } else {
+                                      isAccepted = false;
+                                    }
+                                  });
+                                },
+                              ),
+                              CustomText(
+                                text: 'agree'.tr,
+                                size: 12,
+                              ),
+                            ],
+                          ),
+                          // CustomGameButton(
+                          //   onTap: isAccepted
+                          //       ? () async {
+                          //           LocalStorage.instance
+                          //               .write(first, 'notfirst');
+                          //           Navigator.pop(context);
+                          //         }
+                          //       : null,
+                          //   text: 'accept'.tr,
+                          //   fontSize: 12,
+                          // ),
+                          SizedBox(
+                            height: 35,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateColor.resolveWith((states) =>
+                                          isAccepted
+                                              ? Colors.orange
+                                              : AppTheme.greyTicket)),
+                              // ignore: sort_child_properties_last
+                              child: CustomText(
+                                text: 'accept'.tr,
+                                size: 12,
+                              ),
+                              onPressed: isAccepted
+                                  ? () async {
+                                      LocalStorage.instance
+                                          .write(first, 'notfirst');
+                                      Navigator.pop(context);
+                                    }
+                                  : null,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }),
+            );
+          }
+        }
+      } catch (e) {
+        // print("Error fetching SharedPreferences: $e");
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GameWidget<SoccerRun>.controlled(
+        loadingBuilder: (conetxt) => Container(
+          alignment: Alignment.center,
+          color: Colors.white,
+        ),
+        overlayBuilderMap: {
+          MainMenu.id: (_, game) => MainMenu(game),
+          PauseMenu.id: (_, game) => PauseMenu(game),
+          Hud.id: (_, game) => Hud(game),
+          GameOverMenu.id: (_, game) => GameOverMenu(game),
+          SettingsMenu.id: (_, game) => SettingsMenu(game),
+        },
+        initialActiveOverlays: const [MainMenu.id],
+        gameFactory: () => SoccerRun(
+          camera: CameraComponent.withFixedResolution(
+            width: 360,
+            height: 180,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:soccer_run/screens/game_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (BuildContext context) => const GameScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 100),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
